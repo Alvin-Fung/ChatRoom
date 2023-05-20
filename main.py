ce35abda692f3215734591a27b63c7f129b3bbb5
Alvin-Fung <alvin_fung9@outlook.com>
2023-05-20 14:40:53 +0100

    renamed it to login instead of home, makes more sense in my head

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def generate_unique_code(Length):
     return code
 
 @app.route("/", methods = ["POST","GET"])
-def home():
+def login():
     session.clear()
     if request.method == "POST":
         name = request.form.get("name")
@@ -33,10 +33,10 @@ def home():
         create = request.form.get("create", False)
         
         if not name:
-            return render_template("home.html", error="Please enter a name!", code=code, name=name)
+            return render_template("login.html", error="Please enter a name!", code=code, name=name)
         
         if join != False and not code:
-            return render_template("home.html", error="Please enter a room code!", code=code, name=name)
+            return render_template("login.html", error="Please enter a room code!", code=code, name=name)
         
         #Room creation
         room = code
@@ -44,20 +44,20 @@ def home():
             room = generate_unique_code(4)
             rooms[room] = {"members": 0, "messages": []} #Starting data for room dictionary
         elif code not in rooms: 
-            return render_template("home.html", error="Room does not exist.")
+            return render_template("login.html", error="Room does not exist.")
         
         session["room"] = room #Storing info about a user, instead of using logins for now.
-        session["name"] = room
+        session["name"] = name
         return redirect(url_for("room")) #Redirecting the user to the chat 
         
-    return render_template("home.html")
+    return render_template("login.html")
 
 @app.route("/room")
 def room():
     #Guard clause
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("home"))
+        return redirect(url_for("login"))
     
     return render_template("room.html")
 
