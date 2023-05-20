fcc110f308139bce6a381e974a0d23d589a0a2c6
Alvin-Fung <alvin_fung9@outlook.com>
2023-05-20 15:01:06 +0100

    Connect and disconnect sockets

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def generate_unique_code(Length):
     return code
 
 @app.route("/", methods = ["POST","GET"])
-def login():
+def login(): #Room setup
     session.clear()
     if request.method == "POST":
         name = request.form.get("name")
@@ -61,6 +61,36 @@ def room():
     
     return render_template("room.html")
 
+@socketio.on("connnect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room) #leaves room if it doesn't exist
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room!"}, to = room) #we can use different types of sockets to send to whoever
+    rooms[room]["members"] += 1 #keep track of who is in the room
+    print(f"{name} joined room {room}")
+    
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <=0:
+            del rooms[room]
+    send({"name": name, "message": "has left the room"}, to = room)
+    print(f"{name} has left the room {room}")
+    
+    
 #Initializer
 if __name__ == "__main__":
     socketio.run(app, debug=True)
